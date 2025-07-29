9a5272b7a6fc8177506e41f97ad87c7acde9c329
iacodesing <aguilarirvinv@gmail.com>
2025-07-29 23:19:25 +0200

    intento 2

--- src/Components/GiftItem.jsx
+++ src/Components/GiftItem.jsx
@@ -1,11 +1,12 @@
 
 export const GiftItem = ( { title, url, id} ) => {
 
-
   return (
     <div className="card">
         <img src={url} alt={title} />
         <p>{title}</p>
+        <p>{url}</p>
     </div>
+
   )
 }
